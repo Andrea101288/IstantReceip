e650f48f664b67c561220cd55bf549fd8fc1ac0d
Andrea Mancini <andrea101288@hotmail.it>
2020-05-08 19:11:16 +0200

    Deleting Useless files

--- database/transPy.py
+++
@@ -1,8 +0,0 @@
-import goslate
-
-text = "ciao a tutti\nmi chiamo andrea\ne sono bello\ne mi piace giocare con le bambole"
-
-gs = goslate.Goslate()
-translatedText = gs.translate(text,'en')
-
-print(translatedText)
